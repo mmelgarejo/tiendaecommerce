f6e1cf9dc0a625f8cf93ebca1568c8f137fa0431
mmelgarejo1789 <marcelo.melgarejo1789@gmail.com>
2022-03-10 23:04:55 -0300

    Add view products y roles

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,5 +1,43 @@
 <x-app-layout>
     <div class="container py-8">
-        <h1> {{$product->name}} </h1>
+        <div class="grid grid-cols-2 gap-6">   
+            <div class="flexslider">
+                <ul class="slides">
+                    @foreach ($product->images as $images)
+                        <li>
+                            <img src="{{Storage::url($images->url)}}" />
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+
+            <div>
+                <div class="py-2">
+                    <h1 class="text-lg font-semibold"> {{$product->name}} </h1>
+                </div>
+                <div class="py-2">
+                    <h1 class="text-lg font-semibold"> {{$product->description}} </h1>
+                </div>
+                <div class="py-2">
+                    <h1 class="text-md font-semibold"> GS. {{$product->price}} </h1>
+                </div>
+                <div class="py-2">
+                    <h1 class="text-lg font-semibold"> Stock Disponible: {{$product->quantity}} </h1>
+                </div>
+                <div>
+                    <button class="bg-orange-400 rounded shadow w-full hover:bg-orange-500 font-semibold text-lg py-2 px-4">Agregar producto al carrito</button>
+                </div>
+            </div>
+        </div>
     </div>
+
+    @push('script')
+        <script>
+            $(document).ready(function() {
+                $('.flexslider').flexslider({
+                    animation: "slide"
+                });
+            });
+        </script>
+    @endpush
 </x-app-layout>
